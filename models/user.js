01273211524e1dc742f68af2766fa073b861b884
Evan Jones <evanjones@fastmail.net>
2019-09-28 12:40:47 -0400

    added passport and routes

--- models/user.js
+++ models/user.js
@@ -1,16 +1,37 @@
-module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("User", {
-      // Giving the Author model a name of type STRING
-      name: DataTypes.STRING
+const bcrypt = require('bcryptjs');
+
+module.exports = function (sequelize, DataTypes) {
+  const User = sequelize.define('User', {
+    // Giving the user model a name of type STRING
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  User.associate = function (models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    User.hasMany(models.Review, {
+      onDelete: 'cascade',
     });
-  
-    User.associate = function(models) {
-      // Associating Author with Posts
-      // When an Author is deleted, also delete any associated Posts
-      User.hasMany(models.Review, {
-        onDelete: "cascade"
-      });
-    };
-  
-    return User;
   };
+
+  // Creating a custom method for our User model. This will check if an unhashed password entered by
+  // the user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  User.addHook('beforeCreate', (user) => {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
+  return User;
+};
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,28 @@
-var db = require("../models");
+const db = require('../models');
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+module.exports = function (app) {
+  // Get all users
+  app.get('/api/user', (req, res) => {
+    res.json('test');
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // get most recent posts, for main feed
+  app.get('/api/review', (req, res) => {
+    res.json('test');
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // Create a new review
+  app.post('/api/review', (req, res) => {
+    res.json('test');
+  });
+
+  // update a review by id
+  app.put('api/review/:id', (req, res) => {
+    res.json('test');
+  });
+
+  // Delete a review by id
+  app.delete('/api/review/:id', (req, res) => {
+    res.json('test');
   });
 };
