34a8b5c43de95c3dcefe2899e5853f077d2a5ba6
Leslie Monrose <lesliemonrose@Leslies-MacBook.local>
2019-09-29 09:25:06 -0400

    html files created

---
+++ models/title.js
@@ -0,0 +1,16 @@
+module.exports = function (sequelize, DataTypes) {
+  const Title = sequelize.define('Title', {
+    // Giving the Author model a name of type STRING
+    name: DataTypes.STRING,
+  });
+
+  Title.associate = function (models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    Title.hasMany(models.Review, {
+      onDelete: 'cascade',
+    });
+  };
+
+  return Title;
+};
