9e73c873d74c5846b01c9e35ba3e81548ee8d125
Evan Jones <evanjones@fastmail.net>
2019-09-30 11:11:18 -0400

    Database connections in routes done

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -6,6 +6,7 @@ module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
+  // send it username and password
   app.post('/api/login', passport.authenticate('local'), (req, res) => {
     res.json(req.user);
   });
@@ -13,6 +14,7 @@ module.exports = function (app) {
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully,
   // proceed to log the user in, otherwise send back an error
+  // send it username and password
   app.post('/api/signup', (req, res) => {
     db.User.create({
       username: req.body.username,
@@ -30,20 +32,5 @@ module.exports = function (app) {
   app.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
-  });
-
-  // Route for getting some data about our user to be used client side
-  app.get('/api/user_data', (req, res) => {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        username: req.user.username,
-        id: req.user.id,
-      });
-    }
   });
 };
